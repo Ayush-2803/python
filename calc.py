6f6c9b748607469ea23b7f147b6960d6762b092e
Ayush-2803 <ayush.github@gmail.com>
2024-12-09 18:30:24 +0530

    Added comments to my code to make it more readable

--- calc.py
+++ calc.py
@@ -7,27 +7,36 @@
 # Display the result.
 # Allow the user to perform multiple calculations until they choose to exit.
 
+#function for addition
 def add (a,b):
     return a+b
 
+#function for subtraction
 def subtract (a,b):
     return a-b
 
+#function for multiplication
 def multiply (a,b):
     return a*b
 
+#function for division
 def divide (a,b):
+#division by 0 check
     if b == 0:
         return "Error ! Div by 0"
     else :
         return a/b
-    
+
+#function for menu 
 def display_menu():
     print("\nSimple Calculator")
     print("Select Operation")
     print(" 1 for addition \n 2 for subtraction \n 3 for multiplication \n 4 for division \n 5 for exit ")
 
+#actual function for calculator
 def calculator():
+    
+#loop for the calculator to work everytime until the user quits
     while True:
 
         display_menu()
@@ -38,12 +47,15 @@ def calculator():
             break
         
         if choice in {1,2,3,4}:
+            
+#try except for verifying the right choices
             try:
                 num1 = float(input("Enter a number : "))
                 num2 = float(input("Enter other number : "))
             except ValueError:
                 print("Invalid !!! Enter a valid number.")
 
+#calling the respective functions as per the choices
             if choice == 1:
                 print(f"The resuilt is {add(num1,num2)}")
             elif choice == 2:

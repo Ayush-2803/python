a521d3f18cb033b99e176a268e47a40c216f8cb5
Ayush-2803 <ayush.github@gmail.com>
2025-01-08 10:27:11 +0530

    completed the code

--- password.py
+++ password.py
@@ -6,3 +6,7 @@ alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
 num = ['1','2','3','4','5','6','7','8','9','0']
 symbol = ['!','@','#','$','%','^','&','*','(',')','_']
 
+strong_password = random.choice(alpha) + random.choice(num) + random.choice(symbol)
+for i in range(10):
+    strong_password += random.choice(alpha + num + symbol)
+print("A strong password is: ", strong_password)
